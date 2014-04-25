470042e41a4e1d7201724b49d5b9edd45d273ceb
Ashok Sriram <ashoksriram92@gmail.com>
2014-04-26 00:24:37 +0530

    Adding Guard, Spork support and title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,10 +8,15 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-	end
+  	it "should have the right title" do
+  	  visit '/static_pages/home'
+  	  expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+  	end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
@@ -22,8 +27,8 @@ describe "StaticPages" do
     end
 
     it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  visit '/static_pages/help'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 	end
   end
 
@@ -34,8 +39,8 @@ describe "StaticPages" do
   	end
 
   	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  visit '/static_pages/about'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
 	end
   end
 
