de70501b91191ee93a674f3ab8d6f089b11de370
Ashok Sriram <ashoksriram92@gmail.com>
2014-04-28 00:21:28 +0530

    Factory girl, gravatar, let in rspec, flash messages, forms, param permit, error messages, profile page and sign up logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	id = Integer(params[:id])
+  	@user = User.new
+  	if id <= User.count
+  		@user = User.find(params[:id])
+  	end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
