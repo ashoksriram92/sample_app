19a744a85e89a6062d09868bd29d14c3ef617286
Ashok Sriram <ashoksriram92@gmail.com>
2014-05-02 01:18:58 +0530

    List users page, faker gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:edit, :update]
+	before_action :signed_in_user, only: [:index, :edit, :update]
 	before_action :correct_user,   only: [:edit, :update]
 
   def new
@@ -38,6 +38,10 @@ class UsersController < ApplicationController
 	  render 'edit'
     end
 
+  end
+
+  def index
+  	@users = User.all
   end
 
   private
